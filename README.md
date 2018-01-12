75cd4bdbc56d23f8b5198334c5bcb3ddfb8563f1
Joseph Meli <joemeli631@gmail.com>
2018-01-12 11:56:45 -0500

    created all sub list view, see views folder.

--- README.md
+++ README.md
@@ -1,37 +1,40 @@
 <p align="center"><img src="https://github.com/NERVEUML/Test-runner-webapp/blob/master/src/media/assets/Logo.png"></p>
 
 # Test Runner Web App
+
 GXM rebuild of an app designed to log autonomous robot data. If you want to see more detailed information  on the break down of the app please consult the [wiki](https://github.com/NERVEUML/Test-runner-webapp/wiki)
 
-### In order to see this app run locally on your machine simply
+## Setup
 
-``` $ git clone  git@github.com:NERVEUML/Test-runner-webapp.git ```
+```$ git clone  git@github.com:NERVEUML/Test-runner-webapp.git```
 
 since this branch is still in development make sure to switch to 'single'
 
 ### Prerequisites
 
-[node](https://nodejs.org/en/) and [npm](https://www.npmjs.com/)
+[node](https://nodejs.org/en/), [npm](https://www.npmjs.com/) and [python2](https://wiki.python.org/moin/BeginnersGuide/Download)
 
 ### Usage
 
 Enter the newly cloned directory and start the process of installing all packages and dependencies.
 
-``` $ npm install ```
+```$ npm install```
 
-Make sure to also have [python2](https://wiki.python.org/moin/BeginnersGuide/Download) installed for the SimpleHTTPServer.
-the below command runs:
-``` "cd www; python2 -m SimpleHTTPServer 3000; cd -" ```
+### Run
 
-Run :
-``` $ npm run dev ```
+```$ npm run dev```
+
+npm run dev:
+```cd www; python2 -m SimpleHTTPServer 3000; cd -```
 
 Navigate to  [localhost on port 3000](http://localhost:3000)
 
-### Styles
+## Styles
+
 * Using Semantic UI
 
 ## Built With
+
 * [HTML 5](https://developer.mozilla.org/en-US/docs/Learn/HTML)
 * [JavaScript](https://developer.mozilla.org/en-US/docs/Learn/JavaScript)
 * [Semantic UI](https://semantic-ui.com/)
--- www/js/main.js
+++ www/js/main.js
@@ -1,12 +1,11 @@
 // TODO: mape the objects of Team and Task from locations  and evals to be the same in runs 
 // NOTE: is there a better way to handle the consitent Keys of ( 'runs','configs', 'evals', 'location')
 var allthings = {
-  runs: [],
-  configs: [],
-  evals: [],
-  locations: [],
+	runs: [],
+	configs: [],
+	evals: [],
+	locations: [],
 }
-
 /*
 Description: Runs on page to process what page to show
  Also handles loading LocalStorage loading from old content
@@ -14,14 +13,13 @@ Parameters:
 Return: 
 */
 document.onreadystatechange = () => {
-  console.log("onreadystatechange");
-  showPage(1);
-  if (localStorage.allthings !== undefined) {
-    allthings = returnallfromlocalstorage();
-    rerenderall();
-  }
+	console.log("Step 0");
+	showPage(1);
+	if (localStorage.allthings !== undefined) {
+		allthings = returnallfromlocalstorage();
+		rerenderall();
+	}
 };
-
 // #TODO: Create a way to illustrate that the back button is not an option
 /* 
   Description:
@@ -32,20 +30,17 @@ document.onreadystatechange = () => {
   Returns: Nothing
 */
 function showPage(page) {
-  const pages = ['home', 'evaluationPage', 'evaluationlist',
-    'gpsform', 'configurationPage', 'configurationlist'
-  ];
-  let i = 0;
-  for (i; i < pages.length; i += 1) {
-    if (page === i + 1) {
-      document.getElementById(pages[i]).style.display = 'block';
-      location.hash = `${pages[i]}`;
-    } else {
-      document.getElementById(pages[i]).style.display = 'none';
-    }
-  }
+	const pages = ['home', 'evaluationPage', 'evaluationlist', 'gpsform', 'configurationPage'];
+	let i = 0;
+	for (i; i < pages.length; i += 1) {
+		if (page === i + 1) {
+			document.getElementById(pages[i]).style.display = 'block';
+			location.hash = `${pages[i]}`;
+		} else {
+			document.getElementById(pages[i]).style.display = 'none';
+		}
+	}
 }
-
 /*s
 Description: 
 Parameters: 
@@ -55,10 +50,10 @@ Return:
 setTimeout(hashCheck(window.location.hash), 100);
 
 function hashCheck(hash) {
-  console.log("running func");
-  if (hash != window.location.hash) {
-    console.log("triggered onhaschange: " + window.location.href + " and " + window.location.hash);
-  }
+	console.log("running func");
+	if (hash != window.location.hash) {
+		console.log("triggered onhaschange: " + window.location.href + " and " + window.location.hash);
+	}
 }
 /* 
 Description:
@@ -71,17 +66,16 @@ Parameters: one of 'runs', 'configs', 'evals', 'locations'
 Returns: the key:value object (like {team:"NERVE", task:"1-1A-1"} )
 */
 function getObjectFromForm(idname) {
-  var x = document.getElementById(idname);
-  var e = x.elements;
-  var kvobject = {};
-  for (var i = 0; i < e.length; i++) {
-    if (e[i].tagName == "BUTTON" || e[i].type == "submit") continue;
-    console.log(`${e[i].name} : ${e[i].value} from ${e[i]}`);
-    kvobject[e[i].name] = e[i].value;
+	var x = document.getElementById(idname);
+	var e = x.elements;
+	var kvobject = {};
+	for (var i = 0; i < e.length; i++) {
+		if (e[i].tagName == "BUTTON" || e[i].type == "submit") continue;
+		kvobject[e[i].name] = e[i].value;
   }
-  return kvobject;
+  console.log("Step 1");
+	return kvobject;
 }
-
 /* 
 Description:
   Save a form to the master storage object (global).
@@ -91,19 +85,20 @@ Parameters: one of 'runs', 'configs', 'evals', 'locations'
 Returns: nothing
 */
 function saveToArray(thingtosave) {
-  var nameformidmap = {
-    'runs': 'runform',
-    'evals': 'evaluationForm',
-    'configs': 'configurationForm',
-  } //TODO add to here for locations
+	var nameformidmap = {
+		'runs': 'runform',
+		'evals': 'evaluationForm',
+		'configs': 'configurationForm',
+	} //TODO add to here for locations
   event.preventDefault();
-  console.log(thingtosave);
-  var o = getObjectFromForm(nameformidmap[thingtosave]);
+  console.log("Step 2");
+	console.log(thingtosave);
+	var o = getObjectFromForm(nameformidmap[thingtosave]);
   allthings[thingtosave].push(o);
-  savealltolocalstorage();
-  rerenderall();
+  console.log(o);
+	savealltolocalstorage();
+	rerenderall();
 }
-
 /*
 Description:
   removeElementFromAllThings -> delete one element from the thingtype array and re-saves to localStorage
@@ -113,39 +108,40 @@ Parameters:
 returns: thing removed
 */
 function deleteElementFromAllThings(thingtype, idx) {
-  var x = allthings[thingtype].splice(idx, 1);
-  rerenderall(); //TODO - make this better?
-  savealltolocalstorage();
-  return x;
+  console.log("Step 6")
+	var x = allthings[thingtype].splice(idx, 1);
+	rerenderall(); //TODO - make this better?
+	savealltolocalstorage();
+	return x;
 }
-
 /*
 Description: Function for master rerender
 Parameters: 
 Return: 
 */
 function rerenderall() {
+  console.log("Step 4");
   rerenderElements('runs');
+  rerenderElements('configs');
 }
-
 /*
 Description: saves global array to localStorage 
 Parameters: 
 Return: 
 */
 function savealltolocalstorage() {
-  localStorage.setItem("allthings", JSON.stringify(allthings));
+  console.log("Step 3")
+	localStorage.setItem("allthings", JSON.stringify(allthings));
 }
-
 /*
 Description: loads the allthings localStorage object to the allthings gloabl objects
 Parameters: 
 Return: 
 */
 function returnallfromlocalstorage() {
-  return JSON.parse(localStorage.getItem("allthings"));
+  console.log("Step 5")
+	return JSON.parse(localStorage.getItem("allthings"));
 }
-
 /*
 Description:  rerenders the specific elements by key value 
 Parameters:  a key value of ( 'runs', 'configs', 'evals', 'locations' )
@@ -153,24 +149,27 @@ Return:
 */
 // REVIEW: there must be a better way to solve this then the IFs
 function rerenderElements(kv) {
-  var nameformidmap = {
-    'runs': 'runlist',
-    'evals': 'evaluationlist',
-    'configs': 'configurationlist',
-  } 
-  var list = document.getElementById(nameformidmap[kv]);
-  list.innerHTML = "";
-  if(kv === 'runs'){
-    createRunElements();
-  } else if( kv == 'configs'){
-    createConfigElements();
-  }else if( kv == 'evals'){
-    createEvalElements()
-  }else if( kv == 'locations'){
-    createLocationElements()
-  }
+	var nameformidmap = {
+		'runs': 'runlist',
+		'evals': 'evaluationlist',
+		'configs': 'configurationlist',
+	}
+	var list = document.getElementById(nameformidmap[kv]);
+	list.innerHTML = "";
+	if (kv === 'runs') {
+    console.log("Step 4.runs");
+		createRunElements();
+	} else if (kv == 'configs') {
+    console.log("Step 4.configs");
+		createConfigElements();
+	} else if (kv == 'evals') {
+    console.log("Step 4.evals");
+		createEvalElements()
+	} else if (kv == 'locations') {
+    console.log("Step 4.locations");
+		createLocationElements()
+	}
 }
-
 /*
 Description: 
 Parameters: 
@@ -178,87 +177,174 @@ Return:
 */
 // TODO: Create a function to create all configs, evals, and locations
 function createRunElements() {
-  var runList = document.getElementById('runlist');
-  for (x = 0; x < allthings.runs.length; x += 1) {
-    let teamValue = allthings.runs[x].team;
-    let taskValue = allthings.runs[x].task;
-    var template = `
+  console.log("Step 4.runs.create");
+	var runList = document.getElementById('runlist');
+	for (x = 0; x < allthings.runs.length; x += 1) {
+		let teamValue = allthings.runs[x].team;
+		let taskValue = allthings.runs[x].task;
+		var template = `
     <!--- Start of a single Run -->
-    <div id='run${x}' class=" ui five column grid  segment">
-      <div class="stretched row">
-          <div class="column">
-              <div id='team' class="ui  ">${teamValue}</div>
-          </div>
-          <div class="column">
-              <div id='task' class="ui ">${taskValue}</div>
-          </div>
-          <div class="column">
-              <button class="ui blue button" onclick="showPage(4)">GPS</button>
-          </div>
-          <div class="column">
-              <button class="ui blue button" onclick="showPage(2)">Edit</button>
-          </div>
-          <div class="column">
-            <button class="ui blue button" onclick="deleteElementFromAllThings('runs',${x})">Delete</button>
-          </div>
-      </div>
-    </div>
+   <div id="run${x}">
+                <div class="ui two top attached buttons">
+                        <button class="ui green button" onclick="showPage(4)">GPS</button>
+                    </div>
+                <div class=" ui two column grid  attached  segment">
+                <div class="column">
+                    <div class="ui blue  large label">
+                            ${teamValue}
+                    </div>
+                    <br />
+                    <div class="ui  small label">
+                        Team
+                    </div>
+                </div>
+                <div class="column">
+                    <div class="ui blue  large label">
+                            ${taskValue}
+                    </div>
+                    <br/>
+                    <div class="ui  small label">
+                        Task
+                    </div>
+                </div>
+            </div>
+            <div class="ui two bottom attached buttons">
+                    <button class="ui red button" onclick="deleteElementFromAllThings('runs',${x})">Delete</button>
+                    <button class="ui purple button" onclick="showPage(2)">Edit</button>
+            </div>
+        </div>
     <!-- End of a single Run -->
     `;
-    if (x === 0) {
-      runList.innerHTML = template;
-    } else if (x !== null || x > 0) {
-      document.getElementById(`run${x - 1}`).insertAdjacentHTML('afterend', template);
-    }
-  }
+		if (x === 0) {
+			runList.innerHTML = template;
+		} else if (x !== null || x > 0) {
+			document.getElementById(`run${x - 1}`).insertAdjacentHTML('afterend', template);
+		}
+	}
 }
 
-function createConfigElements (){
-  var configurationlist = document.getElementById('configurationlist');
-  for (x = 0; x < allthings.configs.length; x += 1) {
-    let teamValue = allthings.configs[x].team;
-    let nameValue = allthings.configs[x].name;
-    let rotorsValue = allthings.configs[x].rotors;
-    let batteryValue = allthings.configs[x].battery;
-    let flightControllerValue = allthings.configs[x].flightController;
-    let heightValue = allthings.configs[x].height;
-    let weightValue = allthings.configs[x].weight;
-    let notesValue = allthings.configs[x].notes;
-    var template = `
+// DEBUG: function is not being called at all 
+function createConfigElements() {
+  console.log("Step 4.configs.create");
+	var configurationlist = document.getElementById('configurationlist');
+	for (x = 0; x < allthings.configs.length; x += 1) {
+		let teamValue = allthings.configs[x].team;
+		let nameValue = allthings.configs[x].name;
+		let rotorsValue = allthings.configs[x].rotors;
+		let batteryValue = allthings.configs[x].battery;
+		let flightControllerValue = allthings.configs[x].flightController;
+		let heightValue = allthings.configs[x].height;
+		let weightValue = allthings.configs[x].weight;
+		let notesValue = allthings.configs[x].notes;
+    var template = `<div id="config${x}" class="ui four column grid  attached  blue segment">
+    <div class="column">
+        <div class="ui blue  large label">
+                ${teamValue}
+        </div>
+        <br />
+        <div class="ui  small label">
+            Team
+        </div>
+    </div>
+    <div class="column">
+        <div class="ui blue  large label">
+                ${nameValue}
+        </div>
+        <br/>
+        <div class="ui  small label">
+            Name
+        </div>
+    </div>
+    <div class="column">
+        <div class="ui blue large label">
+                ${rotorsValue}
+        </div>
+        <br/>
+        <div class="ui   small label">
+            Rotors
+        </div>
+    </div>
+    <div class="column">
+            <div class="ui blue large label">
+                    ${batteryValue}
+            </div>
+            <br/>
+            <div class="ui   small label">
+                Battery
+            </div>
+        </div>
+    <div class="column">
+        <div class="ui blue large label">
+            ${flightControllerValue}
+        </div>
+        <br/>
+        <div class="ui small label">
+            Flight Controller
+        </div>
+    </div>
+    <div class="column">
+        <div class="ui blue large label">
+           ${heightValue}
+        </div>
+        <br/>
+        <div class="ui  label">
+            Height
+        </div>
+    </div>
+    <div class="column">
+        <div class="ui blue large label">
+            ${weightValue}
+        </div>
+        <br/>
+        <div class="ui  label">
+            Weight
+        </div>
+    </div>
+    <div class="column">
+        <div class="ui blue large label">
+            ${Notes}
+        </div>
+        <br/>
+        <div class="ui  label">
+            Notes
+        </div>
+    </div>
+</div>
+<div class="ui two bottom attached buttons">
+    <button class="ui  red button" onclick="deleteElementFromAllThings('configs',${x}) ">Delete</button>
+    <button class="ui  purple button" onclick="showPage(5)">Edit</button>
+</div>
+    
+    `;
+		if (x === 0) {
+			configurationlist.innerHTML = template;
+		} else if (x !== null || x > 0) {
+			document.getElementById(`configs${x - 1}`).insertAdjacentHTML('afterend', template);
+		}
+	}
+}
+
+function createEvalElements() {
+	var evlauationlist = document.getElementById('evaluationlist');
+	for (x = 0; x < allthings.evals.length; x += 1) {
+		let teamValue = allthings.evals[x].team;
+		let taskValue = allthings.evals[x].task;
+		let resultValue = allthings.evals[x].result;
+		let percentValue = allthings.evals[x].percent;
+		let flightControllerValue = allthings.evals[x].flightController;
+		let configValue = allthings.evals[x].config;
+		let timeValue = allthings.evals[x].time;
+		let goaltimeValue = allthings.evals[x].goaltime;
+		let notesValue = allthings.evals[x].notes;
+		var template = `
   
     `;
-    if (x === 0) {
-      configurationlist.innerHTML = template;
-    } else if (x !== null || x > 0) {
-      document.getElementById(`configs${x - 1}`).insertAdjacentHTML('afterend', template);
-    }
-  }
-
+		if (x === 0) {
+			evaluationlist.innerHTML = template;
+		} else if (x !== null || x > 0) {
+			document.getElementById(`evals${x - 1}`).insertAdjacentHTML('afterend', template);
+		}
+	}
 }
 
-function createEvalElements (){
-  var evlauationlist = document.getElementById('evaluationlist');
-  for (x = 0; x < allthings.configs.length; x += 1) {
-    let teamValue = allthings.configs[x].team;
-    let taskValue = allthings.configs[x].task;
-    let resultValue = allthings.configs[x].result;
-    let percentValue = allthings.configs[x].percent;
-    let flightControllerValue = allthings.configs[x].flightController;
-    let configValue = allthings.configs[x].config;
-    let timeValue = allthings.configs[x].time;
-    let goaltimeValue = allthings.configs[x].goaltime;
-    let notesValue = allthings.configs[x].notes;
-    var template = `
-  
-    `;
-    if (x === 0) {
-      evaluationlist.innerHTML = template;
-    } else if (x !== null || x > 0) {
-      document.getElementById(`evals${x - 1}`).insertAdjacentHTML('afterend', template);
-    }
-  }
-  
-}
-function createConfigElements (){
-  
-}
+function createConfigElements() {}
